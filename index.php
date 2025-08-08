356c327eb340a0e279d7dc4d877cefee74659061
Stepan <Stepan.Isakhin@infotecs.ru>
2025-08-08 16:53:23 +0500

    change-bd-config

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ПрессФорм - Изготовление и ремонт пресс-форм</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/cleaned.css">
+    <link rel="stylesheet" href="css/style.css">
     
 </head>
 <body>
